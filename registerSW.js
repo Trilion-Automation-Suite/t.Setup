e0cf753d7b0eb03d6742d66511f549df491af358
Charles Amyot <charles@trilion.com>
2026-04-07 11:25:45 -0700

    v0.1.0 Deployment scripts.

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Program%20Files/Git/t.Setup/sw.js', { scope: '/Program%20Files/Git/t.Setup/' })})}
