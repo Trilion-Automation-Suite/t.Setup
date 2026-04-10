a6db80681c807a035eaa1b0e393ff10e99c8123d
Charles Amyot <charles@trilion.com>
2026-04-09 18:37:06 -0700

    v0.2.0 adding a workspace

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/t.Setup/sw.js', { scope: '/t.Setup/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
